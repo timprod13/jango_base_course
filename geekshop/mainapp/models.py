a71d573d2e42b53b3ccc131d4636feda5715b14f
Artemiy Angelyuk <timoha_defn@mail.ru>
2021-07-02 01:40:18 +0700

    l2 complete

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,3 +1,35 @@
+import uuid as uuid
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, verbose_name='uuid')
+    name = models.CharField(max_length=100, verbose_name='категория')
+    created = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    updated = models.DateField(auto_now=True, verbose_name='дата изменения')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, verbose_name='uuid')
+    name = models.CharField(max_length=100, verbose_name='продукт')
+    description = models.TextField(blank=True, verbose_name='описание')
+    price = models.DecimalField(max_digits=1000000, decimal_places=2, verbose_name='цена')
+    image = models.ImageField(verbose_name='изображение')
+    created = models.DateField(auto_now_add=True, verbose_name='дата создания')
+    updated = models.DateField(auto_now=True, verbose_name='дата изменения')
+    category = models.ForeignKey(Category, verbose_name='категория', on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+    def __str__(self):
+        return self.name
